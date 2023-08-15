808b27a2fccb8b466b8d73d8ca898488f177d48a
Tigole <sylvain.janniere@laposte.net>
2023-08-16 00:08:37 +0200

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include "Lys/Lys.hpp"
 
 using namespace std;
 
+
+class PokemonChallengeApplication : public lys::Application
+{
+public:
+    PokemonChallengeApplication(int argc, char* argv[]);
+
+private:
+
+    std::size_t mt_Initialize(void) override;
+};
+
+PokemonChallengeApplication::PokemonChallengeApplication(int argc, char* argv[]) :
+    lys::Application("Pokemon Challenge", lys::CommandLineArguments(argc, argv))
+{}
+
+std::size_t PokemonChallengeApplication::mt_Initialize(void) 
+{
+    return -1;
+}
+
 int main(int argc, char** argv)
 {
-    cout << "hello world!" << endl;
-    return 0;
+    PokemonChallengeApplication l_Application(argc, argv);
+
+    return l_Application.mt_Run();
 }
